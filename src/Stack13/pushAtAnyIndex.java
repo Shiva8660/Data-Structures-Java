775fa437d756e50b0799964420925bbb7133f34f
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-08 21:21:20 +0530

    Code for push at any Index..

---
+++ src/Stack13/pushAtAnyIndex.java
@@ -0,0 +1,35 @@
+package Stack13;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class pushAtAnyIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> orgStack = new Stack();
+
+        orgStack.push(10);
+        orgStack.push(20);
+        orgStack.push(30);
+        orgStack.push(40);
+        orgStack.push(50);
+
+        System.out.println(orgStack);
+
+        System.out.println("Enter index where you want to push : " );
+        int idx = sc.nextInt();
+
+        Stack<Integer> tempStack = new Stack<>();
+        while(orgStack.size() > idx){
+            tempStack.push(orgStack.pop());
+        }
+        System.out.println("Enter the element which you want to push at " + idx +":" );
+        int data = sc.nextInt();
+
+        orgStack.push(data);
+        while(tempStack.size() > 0){
+            orgStack.push(tempStack.pop());
+        }
+        System.out.println("After pushing ..");
+        System.out.println(orgStack);
+    }
+}
