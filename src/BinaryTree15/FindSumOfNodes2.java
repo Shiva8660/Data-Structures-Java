601f13577866763cb3086f01fc465a64bf374bde
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-24 19:19:19 +0530

    Find the sum of all Nodes ..

---
+++ src/BinaryTree15/FindSumOfNodes2.java
@@ -0,0 +1,25 @@
+package BinaryTree15;
+
+public class FindSumOfNodes2 {
+
+    public static int findSumOfNodes(Node root){
+        if(root == null) return 0;
+        int sum = root.val + findSumOfNodes(root.left) + findSumOfNodes(root.right);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1); // a is the root node
+        Node b = new Node(4);
+        Node c = new Node(3);
+        Node d = new Node(2);
+        Node e = new Node(6);
+        Node f = new Node(5);
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+
+        System.out.println("The sum of nodes is : " + findSumOfNodes(a));
+    }
+}
