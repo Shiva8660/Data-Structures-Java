1cf13781dbee58090742d3a27f9b48d9ff9f6ac6
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-25 10:14:22 +0530

    Find products of Tree Nodes..

---
+++ src/BinaryTree15/FindProductsOfNodes3.java
@@ -0,0 +1,26 @@
+package BinaryTree15;
+
+public class FindProductsOfNodes3 {
+
+    public static int findProductOfNodes(Node root){
+        if(root == null) return 1;
+        int product = root.val * findProductOfNodes(root.left) * findProductOfNodes(root.right);
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(2); // a is the root node
+        Node b = new Node(2);
+        Node c = new Node(2);
+        Node d = new Node(2);
+        Node e = new Node(2);
+        Node f = new Node(2);
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+
+        System.out.println("The product of nodes is : " + findProductOfNodes(a));
+    }
+}
+
