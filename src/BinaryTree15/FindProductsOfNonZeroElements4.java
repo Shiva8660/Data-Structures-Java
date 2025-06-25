7ec7492392f24415b834ec6740d6e6fd8af6d838
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-25 10:23:43 +0530

    Some changes in recent code

--- src/BinaryTree15/FindProductsOfNonZeroElements4.java
+++ src/BinaryTree15/FindProductsOfNonZeroElements4.java
@@ -2,10 +2,30 @@ package BinaryTree15;
 
 public class FindProductsOfNonZeroElements4 {
 
-    public static int findProductOfNodes(Node root){
-        if(root == null || root.val == 0) return 1;
-        return  root.val * findProductOfNodes(root.left) * findProductOfNodes(root.right);
+//    public static int findProductOfNodes(Node root){
+//        if(root == null ) return 1;
+//        int left = findProductOfNodes(root.left);
+//        int right = findProductOfNodes(root.right);
+//
+//        // Multiply only if root.val is not zero
+//        return (root.val == 0 ? 1 : root.val) * left * right;
+//    }
+public static int findProductOfNodes(Node root){
+    if(root == null) return 1;
+
+    int left = findProductOfNodes(root.left);
+    int right = findProductOfNodes(root.right);
+
+    int product;
+    if (root.val == 0) {
+        product = 1;
+    } else {
+        product = root.val;
     }
+
+    return product * left * right;
+}
+
 
     public static void main(String[] args) {
         Node a = new Node(2); // a is the root node
