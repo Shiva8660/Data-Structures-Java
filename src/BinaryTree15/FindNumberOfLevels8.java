bf3d3dc647c048dd8c4e60f6fcaf50e263a447ea
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-25 11:41:01 +0530

    Find Number of Levels in Binary Tree..

---
+++ src/BinaryTree15/FindNumberOfLevels8.java
@@ -0,0 +1,32 @@
+package BinaryTree15;
+
+public class FindNumberOfLevels8 {
+
+    public static int CountNumberOfLevels(Node root){
+        if(root == null) return 0;
+
+        return 1 + Math.max(CountNumberOfLevels(root.left) , CountNumberOfLevels(root.right));
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+        c.left = g;
+
+        System.out.println("The total number of level's is : " + CountNumberOfLevels(a));
+    }
+}
+
+
+
+
