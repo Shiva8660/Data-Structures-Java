e6ff7d7e619f1847b8fa872b2fe207f4405a3c31
Shiva7785 <shivanandreddy146@gmail.com>
2025-06-25 11:36:13 +0530

    Count Nodes in Binary Tree..

---
+++ src/BinaryTree15/FindNumberOfNodes7.java
@@ -0,0 +1,31 @@
+package BinaryTree15;
+
+public class FindNumberOfNodes7 {
+
+    public static int CountNodes(Node root){
+        if(root == null) return 0;
+
+        return 1 + CountNodes(root.left) + CountNodes(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1); // a is the root node
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+
+
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+        c.left = g;
+
+        System.out.println("The total number of nodes is : " + CountNodes(a));
+    }
+}
+
+
+
