595a1dcba59b177c829ee77a35b9b502b9468cf6
Shiva7785 <shivanandreddy146@gmail.com>
2025-07-16 19:16:45 +0530

    pairing friends using dp with tabulation

--- src/DP21/FriendsPairingProblem1.java
+++ src/DP21/FriendsPairingProblem1.java
@@ -26,6 +26,36 @@ package DP21;
 //}
 
 // now using dp
+//import java.util.Scanner;
+//import java.util.Arrays;
+//
+//public class FriendsPairingProblem1 {
+//
+//    static class FriendsPairCounter {
+//        public static int pairing(int n, int[] dp) {
+//            if (n <= 2) return n;
+//            if (n == 3) return 4;
+//
+//            if (dp[n] != -1) return dp[n];
+//            return dp[n] = (pairing(n - 1, dp) + (n - 1) * pairing(n - 2, dp));
+//        }
+//
+//        public long countFriendsPairings(int n) {
+//            int[] dp = new int[n + 1];
+//            Arrays.fill(dp, -1);
+//            return pairing(n, dp);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int numberOfFriends = scanner.nextInt();
+//        FriendsPairCounter counter = new FriendsPairCounter();
+//        System.out.println(counter.countFriendsPairings(numberOfFriends));
+//    }
+//}
+
+// now iterative - tabulation
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -33,11 +63,13 @@ public class FriendsPairingProblem1 {
 
     static class FriendsPairCounter {
         public static int pairing(int n, int[] dp) {
-            if (n <= 2) return n;
-            if (n == 3) return 4;
+            dp[1] = 1;
+            dp[2] = 2;
 
-            if (dp[n] != -1) return dp[n];
-            return dp[n] = (pairing(n - 1, dp) + (n - 1) * pairing(n - 2, dp));
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i-1] + (i-1)*dp[i-2];
+            }
+            return dp[n];
         }
 
         public long countFriendsPairings(int n) {
