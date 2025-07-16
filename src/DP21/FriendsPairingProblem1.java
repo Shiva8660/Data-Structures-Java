b0433dee0338dafef4d58f63b02b260d85119440
Shiva7785 <shivanandreddy146@gmail.com>
2025-07-16 09:40:21 +0530

    pairing friends using dp

--- src/DP21/FriendsPairingProblem1.java
+++ src/DP21/FriendsPairingProblem1.java
@@ -1,19 +1,49 @@
 package DP21;
+//
+//import java.util.Scanner;
+//
+//public class FriendsPairingProblem1 {
+//
+//    static class FriendsPairCounter {
+//
+//        public static int countPairings(int n) {
+//            if (n <= 2) return n;
+//            if (n == 3) return 4;
+//            return countPairings(n - 1) + (n - 1) * countPairings(n - 2);
+//        }
+//
+//        public long totalWaysToPair(int n) {
+//            return countPairings(n);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int numberOfFriends = scanner.nextInt();
+//        FriendsPairCounter counter = new FriendsPairCounter();
+//        System.out.println(counter.totalWaysToPair(numberOfFriends));
+//    }
+//}
 
+// now using dp
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class FriendsPairingProblem1 {
 
     static class FriendsPairCounter {
-
-        public static int countPairings(int n) {
+        public static int pairing(int n, int[] dp) {
             if (n <= 2) return n;
             if (n == 3) return 4;
-            return countPairings(n - 1) + (n - 1) * countPairings(n - 2);
+
+            if (dp[n] != -1) return dp[n];
+            return dp[n] = (pairing(n - 1, dp) + (n - 1) * pairing(n - 2, dp));
         }
 
-        public long totalWaysToPair(int n) {
-            return countPairings(n);
+        public long countFriendsPairings(int n) {
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, -1);
+            return pairing(n, dp);
         }
     }
 
@@ -21,7 +51,8 @@ public class FriendsPairingProblem1 {
         Scanner scanner = new Scanner(System.in);
         int numberOfFriends = scanner.nextInt();
         FriendsPairCounter counter = new FriendsPairCounter();
-        System.out.println(counter.totalWaysToPair(numberOfFriends));
+        System.out.println(counter.countFriendsPairings(numberOfFriends));
     }
 }
+
 
