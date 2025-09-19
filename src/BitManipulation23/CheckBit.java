5bad725fc84df02accae5a33f0746c533e7ab59e
Shiva7785 <shivanandreddy146@gmail.com>
2025-09-19 17:07:56 +0530

    Kth bit set or not : gfg => Time and Space Complexcity is O(1)

--- src/BitManipulation23/CheckBit.java
+++ src/BitManipulation23/CheckBit.java
@@ -2,34 +2,44 @@ package BitManipulation23;
 
 import java.util.Scanner;
 
+//class CheckBit {
+//    // method 1
+//    static boolean checkKthBit(int n, int k) {
+//        String str = "";
+//
+//        // Build binary string
+//        while (n != 0) {
+//            str += n % 2;
+//            n = n / 2;
+//        }
+//
+//        // Reverse to get correct binary
+//        String reversed = new StringBuilder(str).reverse().toString();
+//        int len = reversed.length();
+//        int[] arr = new int[len];
+//
+//        // ✅ Proper conversion from char to int
+//        for (int i = 0; i < len; i++) {
+//            arr[i] = Character.getNumericValue(reversed.charAt(i));
+//        }
+//
+//        // If k is outside available bits → false
+//        if (k >= len) {
+//            return false;
+//        }
+//
+//        // Otherwise check kth bit
+//        return arr[k] == 1;
+//    }
+
+
+    // method 2
 class CheckBit {
     static boolean checkKthBit(int n, int k) {
-        String str = "";
+        return (n & (1 << k)) != 0;
 
-        // Build binary string
-        while (n != 0) {
-            str += n % 2;
-            n = n / 2;
-        }
-
-        // Reverse to get correct binary
-        String reversed = new StringBuilder(str).reverse().toString();
-        int len = reversed.length();
-        int[] arr = new int[len];
-
-        // ✅ Proper conversion from char to int
-        for (int i = 0; i < len; i++) {
-            arr[i] = Character.getNumericValue(reversed.charAt(i));
-        }
-
-        // If k is outside available bits → false
-        if (k >= len) {
-            return false;
-        }
-
-        // Otherwise check kth bit
-        return arr[k] == 1;
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
